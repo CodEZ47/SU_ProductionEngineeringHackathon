81d42fe29d7142c7997952bdb61196f4c605fb67
Rishika <rishika0707@gmail.com>
2026-04-05 13:29:49 +0530

    Configure CI with PostgreSQL using environment variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,24 @@
 import pytest
 from app import create_app
+from app.database import db
+from app.models.user import User
+from app.models.url import URL
+from app.models.event import Event
+
 
 @pytest.fixture
 def app():
     app = create_app()
     app.config["TESTING"] = True
-    return app
+
+    with app.app_context():
+        db.create_tables([User, URL, Event])
+
+        User.create(id=1, username="test", email="test@test.com")
+
+    yield app
+
 
 @pytest.fixture
 def client(app):
     return app.test_client()
-
